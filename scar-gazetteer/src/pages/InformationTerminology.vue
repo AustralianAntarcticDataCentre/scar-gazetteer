76ce98d23858f84f6ed7ba6deb066d9edc9c8831
Tristan <tristan.harris520@gmail.com>
2025-10-03 15:23:07 +1000

    terminoloy + home page text changes

--- scar-gazetteer/src/pages/InformationTerminology.vue
+++ scar-gazetteer/src/pages/InformationTerminology.vue
@@ -2,20 +2,13 @@
   <div class="span9">
     <h1>Terminology</h1>
     <p>
-      <b>Place ID</b> Sample text sample text sample text sample text sample
-      text sample text
+      <b>Name ID</b>: A unique number identifying a specific geographic feature. A feature can have multiple name IDs because it can have multiple names. Sometimes these names can be the same, indicating a nation has adopted the place names from another country. Sometimes they can be different due to countries adopting different names or the translation of a name from one language to another.
     </p>
     <p>
-      <b>Name ID</b> Sample text sample text sample text sample text sample text
-      sample text
+      <b>Place ID</b>: A unique number identifying a place name approved by a naming authority of a country.
     </p>
     <p>
-      <b>Relic</b> Sample text sample text sample text sample text sample text
-      sample text
-    </p>
-    <p>
-      <b>CGA</b> Sample text sample text sample text sample text sample text
-      sample text
+      <b>Relic</b>: A historical name or the name of a feature that no longer exists (for example, due to a changing coastline).
     </p>
   </div>
 </template>
