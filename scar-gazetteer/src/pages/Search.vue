ec901fdb81de106c91ba537f09b82f4719815da2
Owen Andrews <owen.andrews@aad.gov.au>
2025-10-13 15:32:51 +1100

    Fix gazetteer names

--- scar-gazetteer/src/pages/Search.vue
+++ scar-gazetteer/src/pages/Search.vue
@@ -2,10 +2,10 @@
   <b-container class="search">
     <h1>Search for place names</h1>
     <b-form @submit="search">
-      <b-form-group label="Place Name:" label-for="search_text">
+      <b-form-group label="Place name:" label-for="search_text">
         <b-form-input id="search_text" class="my-0" v-model="form.search_text" type="text" />
       </b-form-group>
-      <b-form-group label="Feature Type:" label-for="feature_type">
+      <b-form-group label="Feature type:" label-for="feature_type">
         <b-form-select id="feature_type" class="form-select" v-model="form.feature_type" :options="feature_types" />
       </b-form-group>
       <b-form-group label="Gazetteer:" label-for="gazetteer">
@@ -14,10 +14,10 @@
       <b-form-group label="Relics:" label-for="relics">
         <b-form-select id="relics" class="form-select" v-model="form.relics" :options="relics" />
       </b-form-group>
-      <b-form-group label="Named After:" label-for="date_after" id="date_after_label">
+      <b-form-group label="Named after:" label-for="date_after" id="date_after_label">
         <b-form-input id="date_after" v-model="form.date_after" type="date" />
       </b-form-group>
-      <b-form-group label="Named Before:" label-for="date_before">
+      <b-form-group label="Named before:" label-for="date_before">
         <b-form-input id="date_before" v-model="form.date_before" type="date" />
       </b-form-group>
       <b-button class="submit-button" type="submit" variant="primary">Search</b-button>
@@ -28,6 +28,7 @@
 <script>
 import qs from "qs";
 import axios from "axios";
+import { getNameForNumericIsoCountryCode } from "@/utils"
 
 export default {
   name: "Search",
@@ -40,11 +41,11 @@ export default {
         },
       ],
       relics: [
-        { value: null, text: "Include Relics" },
-        { value: 1, text: "Exclude Relics" },
-        { value: 2, text: "Only Relics" },
+        { value: null, text: "Include relics" },
+        { value: 1, text: "Exclude relics" },
+        { value: 2, text: "Only relics" },
       ],
-      gazetteers: [{ value: null, text: "All Gazetteers" }],
+      gazetteers: [{ value: null, text: "All gazetteers" }],
       form: {
         search_text: "",
         feature_type: null,
@@ -60,27 +61,28 @@ export default {
       event.preventDefault();
       this.$router.push(`/search/results?${qs.stringify(this.form)}`);
     },
+    async getGazetteers() {
+      let { data } = await axios.get("/api/gazetteers?order=gazetteer_name.asc");
+
+      let formatted = data.map((g) => {
+        return { value: g.gazetteer_code, text: g.gazetteer_name || getNameForNumericIsoCountryCode(g.country_id) || 'Unknown' };
+      });
+
+      this.gazetteers = this.gazetteers.concat(formatted);
+    },
+    async getFeatureTypes() {
+      let { data } = await axios.get("/api/feature_types?feature_type_code=neq.0&order=feature_type_name.asc");
+
+      let formatted = data.map((f) => {
+        return { value: f.feature_type_code, text: f.feature_type_name };
+      });
+
+      this.feature_types = this.feature_types.concat(formatted);
+    }
   },
-  mounted: async function () {
-    let response = await axios.get("/api/gazetteers?order=gazetteer_name.asc");
-    let gaz = response.data;
-
-    let formatted = gaz.map((g) => {
-      return { value: g.gazetteer_code, text: g.gazetteer_name };
-    });
-
-    this.gazetteers = this.gazetteers.concat(formatted);
-
-    let response2 = await axios.get(
-      "/api/feature_types?feature_type_code=neq.0&order=feature_type_name.asc"
-    );
-    let feat = response2.data;
-
-    let formatted2 = feat.map((f) => {
-      return { value: f.feature_type_code, text: f.feature_type_name };
-    });
-
-    this.feature_types = this.feature_types.concat(formatted2);
+  mounted() {
+    this.getGazetteers()
+    this.getFeatureTypes()
   },
 };
 </script>
