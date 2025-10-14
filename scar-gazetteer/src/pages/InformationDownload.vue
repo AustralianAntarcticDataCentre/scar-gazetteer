179167f2500b47812a5e9715178b0bfe4063ac40
Lewis Rockliffe <Lewis.Rockliffe@aad.gov.au>
2025-10-15 10:59:45 +1100

    use postgrest for bulk download

--- scar-gazetteer/src/pages/InformationDownload.vue
+++ scar-gazetteer/src/pages/InformationDownload.vue
@@ -31,6 +31,7 @@
 <script>
 import axios from 'axios'
 import { getNameForNumericIsoCountryCode } from '../utils'
+import download from 'downloadjs'
 
 export default {
     name: "Search",
@@ -38,14 +39,13 @@ export default {
         return {
             form: {
                 gazetteer: null,
-                format: 'CSV',
-                version: 'aadc:SCAR_CGA_PLACE_NAMES'
+                format: 'text/csv'
             },
             gazetteers: [{ value: null, text: "All Gazetteers" }],
             formats: [
-                { value: 'CSV', text: "CSV" },
-                { value: 'application%2Fjson', text: 'GeoJSON' },
-                { value: 'application%2Fvnd.google-earth.kml%2Bxml', text: 'KML' }],
+                { value: 'text/csv', text: "CSV" },
+                { value: 'application/geo+json', text: 'GeoJSON' },
+                { value: 'application/vnd.google-earth.kml+xml', text: 'KML' }],
         }
     },
     methods: {
@@ -55,11 +55,35 @@ export default {
             let filter = ""
 
             if (this.form.gazetteer) {
-                filter = `&CQL_FILTER=gazetteer='${this.form.gazetteer}'`
+                filter = `?gazetteer=eq.${this.form.gazetteer}`
             }
 
-            const downloadLink = `https://data.aad.gov.au/geoserver/aadc/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=aadc:SCAR_CGA_PLACE_NAMES&outputFormat=${this.form.format}${filter}`
-            window.location = downloadLink
+            let url = `/api/place_names${filter}`
+
+            if(this.form.format == 'text/csv') {
+                url = `/api/place_names_consolidated${filter}`
+            }
+
+            const options = {
+            headers: {
+                Accept: this.form.format
+            }
+            };
+            fetch(url, options)
+            .then( res => res.blob() )
+            .then( blob => {
+
+                let gazetteer = this.form.gazetteer || 'All'
+                let fileSuffix = '.csv'
+
+                if (this.form.format == 'application/geo+json') {
+                    fileSuffix = '.json'
+                } else if (this.form.format == 'application/vnd.google-earth.kml+xml') {
+                    fileSuffix = '.kml'
+                }
+
+                download(blob, `SCAR-CGA-${gazetteer}${fileSuffix}`, this.form.format)
+            });
         }
     },
     mounted: async function () {
