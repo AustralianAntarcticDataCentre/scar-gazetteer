40c441e8cf2782eb9f9640abd8febf4d6ab204d8
Owen Andrews <owen.andrews@aad.gov.au>
2025-11-05 18:07:42 +1100

    Swap basemaps for GEBCO tiles

--- scar-gazetteer/src/components/PlaceNameMap.vue
+++ scar-gazetteer/src/components/PlaceNameMap.vue
@@ -2,13 +2,9 @@
     <div>
         <div class="map-controls mb-2">
             <b-button-group size="sm">
-                <b-button :variant="mapProjection === 'EPSG:3031' ? 'secondary' : 'outline-secondary'"
-                    @click="changeProjection('EPSG:3031')">
-                    Polar Stereographic
-                </b-button>
-                <b-button :variant="mapProjection === 'EPSG:4326' ? 'secondary' : 'outline-secondary'"
-                    @click="changeProjection('EPSG:4326')">
-                    Mercator
+                <b-button v-for="projection in projections" :variant="currentProjection == projection ? 'secondary' : 'outline-secondary'" :key="projection.name"
+                    @click="setProjection(projection)">
+                    {{ projection.name }}
                 </b-button>
             </b-button-group>
         </div>
@@ -18,77 +14,79 @@
 
 <script>
 import Feature from 'ol/Feature';
-import GeoJSON from 'ol/format/GeoJSON';
 import Map from 'ol/Map';
 import Point from 'ol/geom/Point';
 import VectorLayer from 'ol/layer/Vector';
 import VectorSource from 'ol/source/Vector';
 import View from 'ol/View';
-import { Style, Fill, Stroke, Icon } from 'ol/style';
+import { Style, Icon, Stroke } from 'ol/style';
 import 'ol/ol.css';
 
 import proj4 from "proj4";
+import { register } from 'ol/proj/proj4.js';
 import { join } from '../utils';
+import TileLayer from 'ol/layer/Tile';
+
+import WMTSCapabilities from 'ol/format/WMTSCapabilities.js';
+import WMTS, {optionsFromCapabilities} from 'ol/source/WMTS.js';
+import { Circle } from 'ol/geom';
+
+const parser = new WMTSCapabilities();
 
 // https://epsg.io/3031
 proj4.defs("EPSG:3031", "+proj=stere +lat_0=-90 +lat_ts=-71 +lon_0=0 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs +type=crs")
 
-const landStyle = new Style({
-    fill: new Fill({
-        color: '#f8f8f8',
-    }),
-    stroke: new Stroke({
-        color: '#333333',
-        width: 0.3
-    }),
-});
+register(proj4)
 
-const iceStyle = new Style({
-    fill: new Fill({
-        color: '#e9ecef',
-    }),
-    stroke: new Stroke({
-        color: '#333333',
-        width: 0.3
-    }),
-});
+const projections = [
+    {
+        name: 'Polar Stereographic',
+        view: new View({
+            center: [0, 0],
+            zoom: 4,
+            maxZoom: 22,
+            extent: [-5000000, -5000000, 5000000, 5000000],
+            //constrainOnlyCenter: false,
+            projection: "EPSG:3031"
+        }),
+        basemap: loadWmtsLayer('https://tiles.arcgis.com/tiles/C8EMgrsFcRFL6LrL/arcgis/rest/services/Antarctic_Basemap/MapServer/WMTS/1.0.0/WMTSCapabilities.xml?cacheKey=a7cac5fc36cf8cca', 'Antarctic_Basemap')
+    },
+    {
+        name: 'Mercator',
+        view: new View({
+            center: [0, -75],
+            zoom: 0,
+            maxZoom: 22,
+        }),
+        basemap: loadWmtsLayer('https://tiles.arcgis.com/tiles/C8EMgrsFcRFL6LrL/arcgis/rest/services/GEBCO_basemap_NCEI/MapServer/WMTS/1.0.0/WMTSCapabilities.xml?cacheKey=a1b48296ec17c253', 'GEBCO_basemap_NCEI')
+    }
+]
 
-const mapStyle = function (feature) {
-    const isLand = feature.get('surface') == 'land';
-    return isLand ? landStyle : iceStyle;
+async function loadWmtsLayer(url, layer) {
+    const response = await fetch(url)
+    const text = await response.text()
+
+    const result = parser.read(text);
+    const options = optionsFromCapabilities(result, { layer })
+
+    return new TileLayer({
+        opacity: 1,
+        source: new WMTS(options),
+    })
 }
 
-const antarcticaLayer = new VectorLayer({
-    source: new VectorSource({
-        format: new GeoJSON(),
-        url: join(process.env.BASE_URL, `/data/antarctica.json`),
-        attributions: 'Data provided by SCAR ADD (As part of BAS Data Catalogue)',
-    }),
-    style: mapStyle
-});
-
-const worldLayer = new VectorLayer({
-    source: new VectorSource({
-        format: new GeoJSON(),
-        url: join(process.env.BASE_URL, `/data/world.json`),
-        attributions: "Data provided by Natural Earth",
-    }),
-    style: mapStyle
-});
-
-const polarStereographicView = new View({
-    center: [0, 0],
-    zoom: 4,
-    maxZoom: 10,
-    extent: [-5000000, -5000000, 5000000, 5000000],
-    constrainOnlyCenter: false
-})
-
-const mercatorView = new View({
-    center: [0, -10018754], // -75° latitude in Web Mercator
-    zoom: 0,
-    maxZoom: 8,
-})
+function createAnnotationsLayer() {
+    return new VectorLayer({
+        source: new VectorSource(),
+        style: new Style({
+            image: new Icon({
+                anchor: [0.5, 1],
+                scale: 0.02,
+                src: join(process.env.BASE_URL, `/static/marker.svg`)
+            })
+        })
+    });
+}
 
 export default {
     props: {
@@ -99,83 +97,68 @@ export default {
     },
     data: () => ({
         map: null,
-        mapProjection: "EPSG:3031",
-        markerSource: new VectorSource(),
+        annotationsLayer: createAnnotationsLayer(),
+        currentProjection: projections[0]
     }),
     methods: {
-        changeProjection(newProjection) {
-            if (this.map && newProjection !== this.mapProjection) {
-                this.mapProjection = newProjection
+        async setProjection(projection) {
+            this.currentProjection = projection
 
-                const layers = this.map.getLayers();
-                layers.clear();
+            if (this.map.getView() == projection.view) return
 
-                if (this.mapProjection === "EPSG:3031") {
-                    this.map.setView(polarStereographicView)
-                    layers.push(antarcticaLayer)
-                } else {
-                    this.map.setView(mercatorView)
-                    layers.push(worldLayer)
-                }
+            this.map.getLayers().clear()
+            this.map.setView(projection.view)
+            this.map.setLayers([await projection.basemap, this.annotationsLayer])
 
-                layers.push(this.markerLayer())
-                this.updateMarker()
-                this.map.render()
-            }
+            this.updateAnnotations()
         },
-        updateMarker() {
-            this.markerSource.clear();
+        updateAnnotations() {
+            const source = this.annotationsLayer.getSource()
+
+            source.clear()
 
             if (this.coordinates && typeof this.coordinates?.longitude === 'number' && typeof this.coordinates?.latitude === 'number') {
-                let markerFeature = null;
-
-                if (this.mapProjection === "EPSG:3031") {
-                    markerFeature = new Feature({
-                        // 4326 is standard, unprojected coordinate system
-                        // 3031 is Antarctic Polar Stereographic
-                        geometry: new Point(proj4("EPSG:4326", "EPSG:3031", [this.coordinates.longitude, this.coordinates.latitude]))
-                    });
-                } else {
-                    markerFeature = new Feature({
-                        // 3857 is Web Mercator
-                        geometry: new Point(proj4("EPSG:4326", "EPSG:3857", [this.coordinates.longitude, this.coordinates.latitude]))
-                    });
-                }
-
-                this.markerSource.addFeature(markerFeature);
+                source.addFeature(new Feature({
+                    // Convert from WGS84 to whatever view we're currently in
+                    geometry: new Point(proj4("EPSG:4326", this.map.getView().getProjection().getCode(), [this.coordinates.longitude, this.coordinates.latitude]))
+                }))
             } else {
                 console.error("Invalid coordinates")
             }
-        },
-        markerLayer() {
-            return new VectorLayer({
-                source: this.markerSource,
-                style: new Style({
-                    image: new Icon({
-                        anchor: [0.5, 1],
-                        scale: 0.02,
-                        src: join(process.env.BASE_URL, `/static/marker.svg`)
-                    })
+
+            // Add a circle with the same colour as the background to hide the janky tile edge
+            if (this.map.getView().getProjection().getCode() === 'EPSG:3031') {
+                const polylineFeature = new Feature({
+                    geometry: new Circle([0, 0], 4529797)
                 })
-            });
-        }
+
+                polylineFeature.setStyle(new Style({
+                    stroke: new Stroke({
+                        color: '#c1c1c1',
+                        width: 6,
+                    })
+                }))
+
+                source.addFeature(polylineFeature)
+            }
+        },
+    },
+    computed: {
+        projections() {
+            return projections
+        },
     },
     watch: {
         coordinates() {
-            this.updateMarker()
-        }
+            this.updateAnnotations()
+        },
     },
-    mounted() {
+    async mounted() {
         this.map = new Map({
             target: this.$refs.map,
-            layers: [
-                antarcticaLayer,
-                this.markerLayer(),
-            ],
-            view: polarStereographicView
         })
 
-        this.updateMarker()
+        this.setProjection(this.currentProjection)
     },
     destroyed() {
         this.map.setTarget(null)
@@ -188,6 +171,6 @@ export default {
 .map-container {
     border: 1px solid var(--bs-dark);
     height: 500px;
-    background-color: #8bcfef;
+    background-color: #c1c1c1;
 }
 </style>
