2b3f37205405f10671c66f8c8540d4a9198eded9
Owen Andrews <owen.andrews@aad.gov.au>
2025-11-25 13:27:43 +1100

    Link to PRNA

--- scar-gazetteer/src/App.vue
+++ scar-gazetteer/src/App.vue
@@ -61,7 +61,7 @@
                     <img src="./assets/scar_logo_sm.png" alt="SCAR logo" class="w-50" />
                 </div>
                 <p>
-                    The SCAR CGA is edited by representatives from Italian National Antarctic Research Program.
+                    The SCAR CGA is edited by representatives from <a href="https://www.pnra.aq/" target="_blank">National Antarctic Research Program</a>.
                 </p>
                 <p class="mb-0">
                     Hosting is provided by the 
