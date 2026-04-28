c335dd5058cecd9983a98e79a0b7ebd435320224
Owen Andrews <owen@owenandre.ws>
2026-04-28 16:42:19 +1000

    Make Vuelidate work again

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue2'
 import path from 'path'
+import EnvironmentPlugin from 'vite-plugin-environment'
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
@@ -9,6 +10,7 @@ export default defineConfig(({ mode }) => {
 
   return {
     plugins: [
+      EnvironmentPlugin({ BUILD: 'web' }), // Only needed for Vuelidate compatibility, can be removed when Vuelidate is upgraded.
       vue()
     ],
     resolve: {
