fd439c62b449328d04450cee6cfe8090ac477e4f
Owen Andrews <owen@owenandre.ws>
2026-04-29 09:11:42 +1000

    Make routes standalone

--- src/main.js
+++ src/main.js
@@ -2,17 +2,47 @@ import Vue from 'vue'
 import { BootstrapVue } from 'bootstrap-vue'
 import VueMeta from 'vue-meta'
 import Vuelidate from 'vuelidate'
+import Router from 'vue-router'
+import Postgrest from 'vue-postgrest'
+
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import '@/assets/main.css'
 
 import store from './store';
 import App from './App.vue'
-import router from './routes/main'
+import routes from './routes'
+import { join } from './utils'
 
 Vue.use(VueMeta)
 Vue.use(BootstrapVue)
 Vue.use(Vuelidate)
+Vue.use(Router)
+Vue.use(Postgrest, {
+    apiRoot: join(import.meta.env.BASE_PATH, `/api`),
+})
+
+const router = new Router({
+    base: import.meta.env.BASE_PATH,
+    mode: 'history',
+    routes: routes
+})
+
+router.beforeEach(async (to, from, next) => {
+    await store.dispatch('user/checkLoggedIn');
+
+    if(to.matched.some(record => record.meta.requiresAdmin)) {
+        if (store.state.user.isAdmin) {
+            next()
+            return
+        }
+
+        next('/')
+        return
+    }
+
+    next()
+})
 
 new Vue({
   router,
---
+++ src/routes/index.js
@@ -0,0 +1,148 @@
+import axios from 'axios'
+
+const Main = () => import("../pages/Main.vue")
+const Search = () => import("../pages/Search.vue")
+const SearchResults = () => import("../pages/SearchResults.vue")
+const PlaceName = () => import("../pages/PlaceName.vue")
+const Information = () => import("../pages/Information.vue")
+const InformationDownload = () => import("../pages/InformationDownload.vue")
+const InformationGeneral = () => import("../pages/InformationGeneral.vue")
+const InformationTerminology = () => import("../pages/InformationTerminology.vue")
+const InformationBatchInstructions = () => import("../pages/InformationBatchInstructions.vue")
+const InformationHistory = () => import("../pages/InformationHistory.vue")
+const InformationThemes = () => import("../pages/InformationThemes.vue")
+const InformationCGACharacteristics = () => import("../pages/InformationCGACharacteristics.vue")
+const InformationData = () => import("../pages/InformationData.vue")
+const InformationCitation = () => import("../pages/InformationCitation.vue")
+const InformationGlossary = () => import("../pages/InformationGlossary.vue")
+const InformationNamingAuthorities = () => import("../pages/InformationNamingAuthorities.vue")
+const InformationStatistics = () => import("../pages/InformationStatistics.vue")
+const NewPlaceName = () => import("../pages/NewPlaceName.vue")
+const EditPlaceName = () => import("../pages/EditPlaceName.vue")
+const NotFound = () => import("../pages/NotFound.vue")
+
+const routes = [
+    {
+        path: '/',
+        name: 'Home',
+        component: Main,
+    },
+    {
+        path: '/information',
+        component: Information,
+        children: [
+            {
+                path: '',
+                component: InformationGeneral
+            },
+            {
+                path: 'terminology',
+                component: InformationTerminology
+            },
+            {
+                path: 'batch-instructions',
+                component: InformationBatchInstructions
+            },
+            {
+                path: 'history',
+                component: InformationHistory
+            },
+            {
+                path: 'themes',
+                component: InformationThemes
+            },
+            {
+                path: 'characteristics',
+                component: InformationCGACharacteristics
+            },
+            {
+                path: 'data-and-validation',
+                component: InformationData
+            },
+            {
+                path: 'citation-information',
+                component: InformationCitation
+            },
+            {
+                path: 'glossary',
+                component: InformationGlossary
+            },
+            {
+                path: 'naming-authorities',
+                component: InformationNamingAuthorities
+            },
+            {
+                path: 'statistics',
+                component: InformationStatistics
+            },
+            {
+                path: 'download',
+                component: InformationDownload
+            },
+        ]
+    },
+    {
+        path: '/search',
+        component: Search,
+        meta: {
+            sitemap: { ignoreRoute: true }
+        }
+    },
+    {
+        path: '/search/results',
+        component: SearchResults,
+        meta: {
+            sitemap: { ignoreRoute: true }
+        }
+    },
+    {
+        path: '/place-names/create',
+        component: NewPlaceName,
+        meta: {
+            requiresAdmin: true,
+            sitemap: { ignoreRoute: true }
+        }
+    },
+    {
+        path: '/place-names/:id',
+        component: PlaceName,
+        meta: {
+            sitemap: {
+                slugs: async () => await getPlaceIds()
+            }
+        }
+    },
+    {
+        path: '/place-names/:id/edit',
+        component: EditPlaceName,
+        meta: {
+            requiresAdmin: true,
+            sitemap: { ignoreRoute: true }
+        }
+    },
+    // Handle legacy internal links
+    {
+        path: '/display_name.cfm',
+        redirect: to => {
+            if (!to.query.gaz_id) return { path: '/search' }
+            return { path: `/place-names/${to.query.gaz_id}` }
+        }
+    },
+    {
+        path: '*',
+        component: NotFound,
+    }
+]
+
+// async function getPlaceIds() {
+//     // const response = await axios.get('https://scartest.data.aad.gov.au/api/place_names?select=name_id', { httpsAgent: agent })
+//     const response = await axios.get('api/place_names?select=name_id')
+
+//     const ids = response.data.map(n => {
+//         return {id: n.name_id}
+//     })
+
+//     return ids
+// }
+
+export default routes
