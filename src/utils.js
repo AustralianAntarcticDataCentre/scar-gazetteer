5e637ae31f50c7cbd6a1d15acf4b43c6bf050349
Owen Andrews <owen@owenandre.ws>
2026-05-04 15:03:08 +1000

    Use a country's short name if available

--- src/utils.js
+++ src/utils.js
@@ -3,7 +3,7 @@ import { byNumeric as isoCountryByNumeric } from 'iso-country-codes'
 export function getNameForNumericIsoCountryCode(code) {
     code = String(code).padStart(3, '0')
     if (Object.prototype.hasOwnProperty.call(isoCountryByNumeric, code)) {
-        return isoCountryByNumeric[code].name || isoCountryByNumeric[code].name
+        return isoCountryByNumeric[code].shortName || isoCountryByNumeric[code].altName || isoCountryByNumeric[code].name
     }
 
     console.error(`Unknown ISO country code: '${code}'`)
