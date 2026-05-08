13980f87c4ccdbe51ca795c3e6be0d9427ba01dd
Owen Andrews <owen@owenandre.ws>
2026-05-08 15:57:35 +1000

    Update general information

--- src/pages/InformationGeneral.vue
+++ src/pages/InformationGeneral.vue
@@ -10,23 +10,23 @@
             The SCAR Composite Gazetteer of Antarctica is an activity conducted in this framework.</p>
 
         <p>In 1992 the SCAR Working Group on Geodesy and Geographic Information (WG-GGI), during the XXII SCAR meeting
-            in Bariloche, recognized the need for a composite gazetteer of Antarctica to bring some order to the complex
+            in Bariloche, recognised the need for a composite gazetteer of Antarctica to bring some order to the complex
             toponymy of Antarctica. The goal of the work programme which evolved from that discussion was to provide the
             scientific community with two products:</p>
         <ul>
             <li>a compilation of all existing geographic names of Antarctica and</li>
-            <li>a set of guidelines to be followed when proposing new names and when selecting one name from a list of
+            <li><a href="https://scar.org/~documents/route%3A/download/5729" target="_blank">a set of guidelines</a> to be followed when proposing new names, and when selecting one name from a list of
                 synonyms for a given feature.</li>
         </ul>
 
         <p>Because Antarctica does not fall under the sovereignty of any one nation this particular continent is
-            similar, in many respects, to the oceans. In general every country has a recognized body which approves the
+            similar, in many respects, to the oceans. In general every country has a recognised body which approves the
             names of geographic features in the country and also has the power to enforce their use. For Antarctica,
             however, there is no such single naming authority. Individual countries are responsible for their national
             policy on, and authorisation and use of, Antarctic names.</p>
 
         <p>The case of the oceans is however somewhat different because, by an agreement between two international
-            institutions - the International Hydrographic Organization (IHO) and the Intergovernmental Oceanographic
+            institutions - the International Hydrographic Organisation (IHO) and the Intergovernmental Oceanographic
             Commission (IOC) - it has been possible to establish a unique set of names for under-sea features depicted
             on the <a href="https://www.gebco.net">General Bathymetric Chart of the Oceans (GEBCO)</a>.</p>
 
@@ -36,10 +36,8 @@
 
         <p>During 2019-2021 SCAGI prepared a document titled International Principles and Procedures for Antarctic Place
             Names which was published in October 2021 as SCAR Report 41. It recommended international principles and
-            procedures for the use of existing place names and the standardization of new names for features in
-            Antarctica (south of 60°S). The SCAR, through its recommendations, expresses the hope that the present
-            effort will contribute to the adoption in Antarctica of the general principle of 'one name per feature' by
-            all Antarctic place naming authorities.</p>
+            procedures for the use of existing place names and the standardisation of new names for features in
+            Antarctica (south of 60°S).</p>
 
         <p>To support the principles and procedures, SCAGI compiled a <a
                 href="https://scar.org/~documents/route%3A/download/5727">reference list</a> of national Antarctic
